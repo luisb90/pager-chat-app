6df6bb43fa01e953c7b13df3f63b2b5c76d84f9f
Luis <luisbe090@gmail.com>
2020-10-17 21:34:58 -0400

    flesh out not found page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Suspense, lazy, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 import styles from './App.module.css';
-import NotFound from './pages/NotFound/NotFound';
 
 const Login = lazy(() => import('./pages/Login/Login'));
 const Chat = lazy(() => import('./pages/Chat/Chat'));
+const NotFound = lazy(() => import('./pages/NotFound/NotFound'));
 
 const App = () => {
   const [selectedUserName, setSelectedUserName] = useState('');
--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const NotFound = () => {
-  return <div>We aint found shit</div>;
+import Button from '../../components/UI/Button/Button';
+
+import styles from './NotFound.module.css';
+
+const NotFound = ({ history }) => {
+  const handleClick = () => {
+    history.push('/');
+  };
+
+  return (
+    <div className={styles.NotFound}>
+      <div className={styles.Header}>This is not the page you're looking for.</div>
+      <Button onClick={handleClick}>Go home</Button>
+    </div>
+  );
 };
 
 export default NotFound;
